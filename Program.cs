3b06afdcee5e8c2fd3c8a75c52b58f59e2af8a3d
Ashif <reachashif@outlook.com>
2022-11-04 11:38:07 +1100

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_World
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello_World");
+            Console.Write("");
+            Console.ReadKey();
+        }
+    }
+}
